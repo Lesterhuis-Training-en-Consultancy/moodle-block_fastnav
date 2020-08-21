7af7cf24589c03331dfa4fad66cc74ba6ace8a31
LT&C <gemma@ltnc.nl>
2020-08-21 15:48:27 +0200

    Dutch translation files
    Dutch translation files.

---
+++ lang/nl/block_fastnav.php
@@ -0,0 +1,62 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Language file EN
+ *
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ *
+ * @package   moodle-block_fastnav
+ * @copyright 16/07/2020 Mfreak.nl | LdesignMedia.nl - Luuk Verhoeven
+ * @author    Luuk Verhoeven
+ **/
+defined('MOODLE_INTERNAL') || die;
+
+$string['pluginname'] = 'Blok Snelnavigatie';
+$string['fastnav:addinstance'] = 'Voeg het blok snelnavigatie toe';
+$string['fastnav:myaddinstance'] = 'Voeg een nieuwe blok snelnavigatie toe aan Mijn startpagina';
+$string['fastnav:management'] = 'Toestaan configureren van Snelnavigatie';
+$string['privacy:metadata'] = 'Het blok Snelnavigatie slaat geen gegevens op.';
+
+// Buttons.
+$string['btn:edit'] = 'Bewerk';
+$string['btn:add'] = 'Toevoegen';
+$string['btn:update'] = 'Bijwerken';
+$string['btn:confirm'] = 'Ja';
+$string['btn:cancel'] = 'Annuleren';
+
+// Javascript.
+$string['js:confirm_delete_title'] = 'Verwijder item';
+$string['js:confirm_delete_message'] = 'Weet je zeker dat je dit item wilt verwijderen?';
+
+// Headings.
+$string['heading:overview'] = 'Overzicht';
+
+// Table.
+$string['heading:table_icon'] = 'Afbeelding';
+$string['heading:table_name'] = 'Naam';
+$string['heading:table_link'] = 'Link';
+$string['heading:table_sortorder'] = 'Sorteren';
+$string['heading:table_action'] = 'Actie';
+
+// Forms.
+$string['form:name'] = 'Naam';
+$string['form:link'] = 'Link';
+$string['form:link_icon'] = 'Afbeelding';
+$string['form:show_block_and_sidebar'] = 'Toon blok en zijbalk op activiteiten pagina\'s';
+$string['form:show_sidebar'] = 'Toon zijbalk alleen op activiteiten pagina\'s';
+$string['form:show_block'] = 'Toon blok alleen op activiteiten pagina\'s';
+$string['form:display_modus'] = 'Weergavemodus';

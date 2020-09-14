e8bad1b8fe9d5c8cc81ae4f5a8cd8bf5581f4dd3
Luuk Verhoeven <luuk@ldesign.nl>
2020-09-14 16:59:13 +0200

    Bump version number

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
  **/
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2020072102;
+$plugin->version = 2020091400;
 $plugin->requires = 2020050600;
 $plugin->component = 'block_fastnav';
 $plugin->maturity = MATURITY_STABLE;

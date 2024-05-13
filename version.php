821dc174695970a02ea806af4d2feb4db48f2808
Luuk Verhoeven <luuk@ldesign.nl>
2024-05-13 21:43:44 +0200

    Update version.php

--- version.php
+++ version.php
@@ -26,9 +26,9 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2024050100;
+$plugin->version = 2024051300;
 $plugin->requires = 2020050600;
-$plugin->supported = [39, 403];
+$plugin->supported = [39, 404];
 $plugin->component = 'block_fastnav';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '4.3.0';
+$plugin->release = '4.4.0';
